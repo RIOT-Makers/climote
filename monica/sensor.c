c602f60b0bc7e15498539f94233eaa2f2fd0c148
smlng <s@mlng.net>
2017-03-09 15:50:29 +0100

    monica: refine sensor measurements

--- monica/sensor.c
+++ monica/sensor.c
@@ -39,8 +39,8 @@ static tmp006_t dev_tmp006;
 #define SENSOR_NUM_SAMPLES      (10U)
 #define SENSOR_THREAD_STACKSIZE (3 * THREAD_STACKSIZE_DEFAULT)
 
-static int samples_humidity[SENSOR_NUM_SAMPLES];
-static int samples_temperature[SENSOR_NUM_SAMPLES];
+static int16_t samples_humidity[SENSOR_NUM_SAMPLES];
+static int16_t samples_temperature[SENSOR_NUM_SAMPLES];
 static mutex_t mutex;
 
 static char sensor_thread_stack[SENSOR_THREAD_STACKSIZE];
@@ -50,16 +50,15 @@ static char sensor_thread_stack[SENSOR_THREAD_STACKSIZE];
  *
  * @return temperature
  */
-int sensor_get_temperature(void)
+int16_t sensor_get_temperature(void)
 {
-    int sum = 0;
+    int32_t sum = 0;
     mutex_lock(&mutex);
     for (unsigned i = 0; i < SENSOR_NUM_SAMPLES; i++) {
         sum += samples_temperature[i];
     }
     mutex_unlock(&mutex);
-    int avg = (sum/SENSOR_NUM_SAMPLES);
-    return avg;
+    return (sum/SENSOR_NUM_SAMPLES);
 }
 
 /**
@@ -67,52 +66,60 @@ int sensor_get_temperature(void)
  *
  * @return humidity
  */
-int sensor_get_humidity(void)
+int16_t sensor_get_humidity(void)
 {
-    int sum = 0;
+    int32_t sum = 0;
     mutex_lock(&mutex);
     for (unsigned i = 0; i < SENSOR_NUM_SAMPLES; i++) {
         sum += samples_humidity[i];
     }
     mutex_unlock(&mutex);
-    int avg = (sum/SENSOR_NUM_SAMPLES);
-    return avg;
+    return (sum/SENSOR_NUM_SAMPLES);
 }
 
-#ifdef MODULE_HDC1000
 /**
  * @brief Measures the temperature and humitity with a HDC1000.
  *
  * @param[out] temp the measured temperature in degree celsius * 100
  * @param[out] hum the measured humitity in % * 100
  */
-static void _hdc1000_measure(int *temp, int *hum) {
+static int16_t _get_humidity(void) {
+    int16_t h;
+#ifdef MODULE_HDC1000
+    int16_t t;
     LOG_DEBUG("[SENSOR] _hdc1000_measure\n");
-    hdc1000_read(&dev_hdc1000, (int16_t *)temp, (int16_t *)hum);
-}
+    hdc1000_read(&dev_hdc1000, &t, &h);
+#else
+    h = (int16_t) random_uint32_range(0, 10000);
 #endif /* MODULE_HDC1000 */
+    return h;
+}
 
-#ifdef MODULE_TMP006
 /**
  * @brief Measures the temperature with a TMP006.
  *
  * @param[out] temp the measured temperature in degree celsius * 100
  */
-static void _tmp006_measure(int *temp)
+static int16_t _get_temperature(void)
 {
-    LOG_DEBUG("[SENSOR] _tmp006_measure\n");
+    LOG_DEBUG("[SENSOR] _get_temperature\n");
+    int16_t t;
+#ifdef MODULE_TMP006
     uint8_t drdy;
     int16_t raw_temp, raw_volt;
     float tamb, tobj;
     /* read sensor, quit on error */
     if (tmp006_read(&dev_tmp006, &raw_volt, &raw_temp, &drdy)) {
-        LOG_ERROR("[SENSOR] tmp006_measure\n");
-        return;
+        LOG_ERROR("[SENSOR] tmp006_read failed\n");
+        return 0;
     }
     tmp006_convert(raw_volt, raw_temp,  &tamb, &tobj);
-    *temp = (int)(tobj*100);
-}
+    t = (int16_t)(tobj*100);
+#else
+    t = (int16_t) random_uint32_range(0, 5000);
 #endif /* MODULE_TMP006 */
+    return t;
+}
 
 /**
  * @brief Intialise all sensores.
@@ -122,7 +129,6 @@ static void _tmp006_measure(int *temp)
 static int _init(void) {
     LOG_DEBUG("[SENSOR] _init\n");
 #ifdef MODULE_HDC1000
-    assert(SENSOR_TIMEOUT_MS > HDC1000_CONVERSION_TIME);
     /* initialise humidity sensor hdc1000 */
     if ((hdc1000_init(&dev_hdc1000, &hdc1000_params[0]) != 0)) {
         LOG_ERROR("[SENSOR] HDC1000 init");
@@ -130,7 +136,6 @@ static int _init(void) {
     }
 #endif /* MODULE_HDC1000 */
 #ifdef MODULE_TMP006
-    assert(SENSOR_TIMEOUT_MS > TMP006_CONVERSION_TIME);
     /* init temperature sensor tmp006 */
     if ((tmp006_init(&dev_tmp006, TMP006_I2C, TMP006_ADDR, TMP006_CONFIG_CR_DEF) != 0)) {
         LOG_ERROR("[SENSOR] TMP006 init");
@@ -144,26 +149,11 @@ static int _init(void) {
         LOG_ERROR("[SENSOR] TMP006 test.");
         return 1;
     }
-    xtimer_usleep(TMP006_CONVERSION_TIME);
-#endif /* MODULE_TMP006 */
-    int h1 = 0;
-    int t1 = 0;
-    int t2 = 0;
-#ifdef MODULE_HDC1000
-    _hdc1000_measure(&t1,&h1);
-#else
-    (void)t1;
-    (void)h1;
-#endif /* MODULE_HDC1000 */
-#ifdef MODULE_TMP006
-    _tmp006_measure(&t2);
-#else
-    (void)t2;
 #endif /* MODULE_TMP006 */
     mutex_lock(&mutex);
     for (unsigned i = 0; i < SENSOR_NUM_SAMPLES; i++) {
-        samples_humidity[i] = h1;
-        samples_temperature[i] = t2;
+        samples_humidity[i]    = _get_humidity();
+        samples_temperature[i] = _get_temperature();
     }
     mutex_unlock(&mutex);
     return 0;
@@ -180,19 +170,12 @@ static void *sensor_thread(void *arg)
     int count = 0;
     xtimer_usleep(SENSOR_TIMEOUT_MS);
     while(1) {
+        /* get latest sensor data */
         mutex_lock(&mutex);
-#ifdef MODULE_TMP006
-        _tmp006_measure(&samples_temperature[count]);
-#else
-        samples_temperature[count] = (int) random_uint32_range(0, 40);
-#endif
-#ifdef MODULE_HDC1000
-        int temp_hdc1000; /* ignored */
-        _hdc1000_measure(&temp_hdc1000, &samples_humidity[count]);
-#else
-        samples_humidity[count] = (int) random_uint32_range(0, 100);
-#endif
+        samples_temperature[count] = _get_temperature();
+        samples_humidity[count] = _get_humidity();
         mutex_unlock(&mutex);
+        /* next round */
         count = (count+1)%SENSOR_NUM_SAMPLES;
         if (count == 0) {
             LOG_INFO("[SENSOR] raw data T: %d, H: %d\n",
