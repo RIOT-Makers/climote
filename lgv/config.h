8c3e4ee9bf9e8370ab43256629a644db9adbbb8c
smlng <s@mlng.net>
2017-05-17 17:11:26 +0200

    lgv: fix path

--- lgv/config.h
+++ lgv/config.h
@@ -5,7 +5,7 @@
 
 #define LGV_PROXY_ADDR          "fd16:abcd:ef21:3::1"
 #define LGV_PROXY_PORT          "5683"
-#define LGV_PATH_TEMPERATURE    "Observations"
+#define LGV_PATH_TEMPERATURE    "/Observations"
 //#define LGV_PATH_HUMITIDY       "/Datastreams(3)/Observations"
 #define LGV_LOOP_WAIT           (10 * US_PER_SEC)
 
